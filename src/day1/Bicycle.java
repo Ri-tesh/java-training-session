3acbe46a362e783a4a664dcae648187f4519b007
Ri-tesh <rritesh.ppradhan@gmail.com>
2022-11-22 10:25:35 -0600

    intial commit

---
+++ src/day1/Bicycle.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Bicycle {
+    // States
+    // Brand Name
+    // Frame Dimensions
+    // Tyre Dimensions
+    // Tyre Types
+    // Drivetrain(Gears)
+    // color
+
+
+
+    // Behaviour
+}
---
+++ src/day1/TelevisionState.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class TelevisionState {
+    // State
+    // Brand Names
+    // Size
+    // Weight
+    // Color
+    // Dimensions
+    // Screen Resolutions
+    // Sound System
+    //Behaviour
+}
