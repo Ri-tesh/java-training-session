56fb0e86bf6f270e9449c2ef41d1b24ca5fc0aad
Ri-tesh <rritesh.ppradhan@gmail.com>
2022-11-23 02:06:12 -0600

    initial commit

---
+++ src/day1/Guitar.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Guitar {
+    // States
+    // Brand Name
+    // Body Type
+    // Wood Material
+    // Guitar Type
+    // Pickups
+    // Strings Brand
+    // Year Made
+    // Body Size
+
+
+    // Behaviours
+}
